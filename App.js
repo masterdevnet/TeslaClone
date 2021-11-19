7b1904ef64e1c5f89b08ac1f13afdf71dc857df8
EstevamNet <masterdev.net@gmail.com>
2021-11-19 10:55:10 -0300

    Inciado repositorio e estilazaçao do App.js

--- App.js
+++ App.js
@@ -5,7 +5,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+    <View style={styles.carContainer}>
+
+      <View style={styles.titles}>
+        <Text style={styles.title}>Modelo S</Text>
+        <Text style={styles.subtitle}>A partir de $69,000.00</Text>
+      </View>
+
+    </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +24,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  carContainer: {
+    width: '100%',
+    height: '100%',
+  },
+  titles: {
+    marginTop: '30%',
+    width: '100%',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: '500',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#5c5e62'
+  }
 });
