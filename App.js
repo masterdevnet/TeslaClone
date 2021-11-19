50d888f7d5d26d792bc33d0e6166cb0a83cb0ca1
EstevamNet <masterdev.net@gmail.com>
2021-11-19 17:37:28 -0300

    Inciado repositorio e estilazaçao do App.js

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       
     <View style={styles.carContainer}>
+
+      <ImageBackground 
+        source={require('./assets/images/ModelX.jpeg')} 
+        style={styles.image}
+      />
 
       <View style={styles.titles}>
         <Text style={styles.title}>Modelo S</Text>
@@ -42,6 +47,12 @@ const styles = StyleSheet.create({
   },
   subtitle: {
     fontSize: 16,
-    color: '#5c5e62'
+    color: '#5c5e62',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    position: 'absolute',
   }
 });
