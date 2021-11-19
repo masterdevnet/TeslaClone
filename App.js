b0b60a2944824729c9357f58e51fcb7da34268a2
EstevamNet <masterdev.net@gmail.com>
2021-11-19 18:59:08 -0300

    Iniciando a implementaçao de componentes

--- App.js
+++ App.js
@@ -1,25 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import CarsList from './components/CarsList';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      
-    <View style={styles.carContainer}>
-
-      <ImageBackground 
-        source={require('./assets/images/ModelX.jpeg')} 
-        style={styles.image}
-      />
-
-      <View style={styles.titles}>
-        <Text style={styles.title}>Modelo S</Text>
-        <Text style={styles.subtitle}>A partir de $69,000.00</Text>
-      </View>
-
-    </View>
-
+    <View style={styles.container} >
+      <Header />
+      <CarsList />
       <StatusBar style="auto" />
     </View>
   );
@@ -29,30 +18,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  carContainer: {
-    width: '100%',
-    height: '100%',
-  },
-  titles: {
-    marginTop: '30%',
-    width: '100%',
     alignItems: 'center',
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: '500',
-  },
-  subtitle: {
-    fontSize: 16,
-    color: '#5c5e62',
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
-    position: 'absolute',
+    justifyContent: 'center',
   }
 });
